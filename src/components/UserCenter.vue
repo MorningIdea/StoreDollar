535fc439f7c66dd8a8b04b942359ea018500dd7a
gong <353105414@qq.com>
2022-07-22 23:42:17 +0800

    修改格式

--- src/components/UserCenter.vue
+++ src/components/UserCenter.vue
@@ -1,17 +1,16 @@
 <template>
   <div class="parentDiv">
-      <h3>User Center</h3>
+      <h2>User Center</h2>
       <p>welcome，{{user}}</p>
-
       <ul id="example-1">
           <li v-for="item in $store.state.dataList">
               <span>{{ item.title }}</span><i>{{ item.dates }}</i><em>{{ item.url }}</em>
           </li>
       </ul>
       <p><a href="javascript:void(0);" @click="logout">退出</a></p>
+      <p></p>
   </div>
 </template>
-
 <script>
   export default {
       data(){
@@ -19,17 +18,14 @@
               list:[]
           }
       },
-
       mounted: function () {
         this.$store.dispatch("showDataList"); //send ajax
       },
-
       computed:{
         user:function (){
           return sessionStorage.getItem("userName")
         }
       },
-
       methods:{
         logout(){
           this.$store.dispatch("logout")
@@ -37,7 +33,6 @@
       }
   }
 </script>
-
 <style>
      *{ padding: 0; margin: 0; font-size: 14px;}
      ul,li,ol{ list-style: none}
